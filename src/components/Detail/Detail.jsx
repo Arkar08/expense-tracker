fd14d64acae3a0536a0138d15f935f889418b91a
Soe Arkar Maung <148836349+Arkar08@users.noreply.github.com>
2024-04-28 17:49:16 +0630

    final

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
+import usrContext from "../../hooks/UsrContext";
 
 const Detail = ({ expense }) => {
-  console.log(expense);
+  const { dispatch } = usrContext();
+  const handleClick = () => {
+    const confirm = window.confirm("Are you want to delete?");
+    if (confirm) {
+      dispatch({ type: "Delete Transcation", payload: expense });
+    }
+  };
   return (
-    <div className="flex  border-2 rounded-md  w-[90%] h-[30px] m-2 mx-auto justify-between items-center px-4 cursor-pointer relative">
+    <div
+      className="flex  border-2 rounded-md  w-[90%] h-[30px] m-2 mx-auto justify-between items-center px-4 cursor-pointer relative"
+      onClick={handleClick}
+    >
       <h2 className="text-xl">{expense.transcation}</h2>
       <h3 className="text-xl">{expense.amount}</h3>
       <div className="h-[100%] bg-green-400 w-[6px] absolute top-0 right-0"></div>
--- src/components/compare/Compare.jsx
+++ src/components/compare/Compare.jsx
@@ -1,16 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import usrContext from "../../hooks/UsrContext";
 
 const Compare = () => {
+  const { expense } = usrContext();
+  const amount = expense.map((e) => e.amount);
+  const income = amount
+    .filter((i) => i > 0)
+    .reduce((e, item) => (e += item), 0)
+    .toFixed(2);
+
+  const expen = (
+    amount.filter((i) => i < 0).reduce((i, item) => (i += item), 0) * -1
+  ).toFixed(2);
   return (
     <div className="flex justify-around items-center mt-4 border w-[90%] mx-auto h-[100%] rounded-md">
       <div className="bg-green-600 w-[50%] rounded-l-md text-center">
-        <h2 className="text-2xl text-white select-none">Cash</h2>
-        <h3 className="text-xl">$500</h3>
+        <h2 className="text-2xl text-white select-none">Income</h2>
+        <h3 className="text-xl">{income}</h3>
       </div>
       <div className="bg-red-600 w-[50%] rounded-r-md text-center">
-        <h2 className="text-2xl text-white select-none">OK$</h2>
-        <h3 className="text-xl">$500</h3>
+        <h2 className="text-2xl text-white select-none">Expense</h2>
+        <h3 className="text-xl">{expen}</h3>
       </div>
     </div>
   );
